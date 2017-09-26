5982d18025317c36f137bc501ffe106f94ae333f
Lemon <papydeath95@gmail.com>
2017-09-26 05:42:13 -0300

    Codigo del comando lyrics movido

--- addons/music.py
+++ addons/music.py
@@ -2,25 +2,36 @@ import config
 from addons.tools.localize import LocalizeMe
 from discord.ext import commands
 
-import genius
 import discord
+import requests
+from bs4 import BeautifulSoup
 
 class Music():
     def __init__(self, bot):
         self.bot = bot
         self.strings = LocalizeMe("strings", config.lang)
-        self.genius = genius.Genius(config.genius)
 
     @commands.command()
     async def lyrics(self, ctx, *, query : str):
-        song = self.genius.search(query)
-        lyrics = self.genius.lyrics(song)
-        embed = discord.Embed(title=self.strings.get("music_lyrics_title").format(song=song.ftitle), description=lyrics, url=song.url)
-        embed.set_thumbnail(url=song.cover)
         try:
-            await ctx.send(embed=embed)
+            r = requests.get(
+                url="https://api.genius.com/search?q=" + query,
+                headers={"Authorization": "Bearer " + config.genius}
+            ).json()["response"]["hits"][0]["result"]
+
+            s = BeautifulSoup(requests.get(r["url"]).text, "html.parser")
+            l = s.find("div", class_="lyrics").get_text().strip()
+
+            embed = discord.Embed(
+                title=self.strings.get("music_lyrics_title").format(song=r["title_with_featured"]),
+                description=l, url=r["url"])
+            embed.set_thumbnail(url=r["header_image_thumbnail_url"])
+        except IndexError:
+            await ctx.send(self.strings.get("music_lyrics_notfound").format(search=query))
         except discord.HTTPException:
-            await ctx.send(self.strings.get("music_lyrics_toobig").format(url=song.url))
+            await ctx.send(self.strings.get("music_lyrics_toobig").format(url=r["url"]))
+        else:
+            await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Music(bot))
