423bfad97ef9a7c8aa7584a0537a8dca6b5ed451
Lemon <papydeath95@gmail.com>
2017-10-10 15:08:14 -0300

    Variable renombrada

--- addons/events.py
+++ addons/events.py
@@ -4,22 +4,22 @@ class Events:
     def __init__(self, bot):
         self.bot = bot
 
-    async def on_command_error(self, ctx, e):
+    async def on_command_error(self, ctx, error):
         if self.bot.dev:
             await ctx.send(self.bot.loc("events_error_development").format(
-                t=type(e), e=e))
+                t=type(error), e=error))
         else:
-            if isinstance(e, commands.MissingRequiredArgument):
-                await ctx.send(self.bot.loc("events_error_pmissing").format(e.param))
-            elif isinstance(e, commands.NoPrivateMessage):
+            if isinstance(error, commands.MissingRequiredArgument):
+                await ctx.send(self.bot.loc("events_error_pmissing").format(error.param))
+            elif isinstance(error, commands.NoPrivateMessage):
                 await ctx.send(self.bot.loc("events_error_pmdisabled"))
-            elif isinstance(e, commands.CommandOnCooldown):
-                await ctx.send(self.bot.loc("events_error_cooldown").format(e.retry_after))
-            elif isinstance(e, commands.NotOwner):
+            elif isinstance(error, commands.CommandOnCooldown):
+                await ctx.send(self.bot.loc("events_error_cooldown").format(error.retry_after))
+            elif isinstance(error, commands.NotOwner):
                 await ctx.send(self.bot.loc("events_error_owneronly"))
-            elif isinstance(e, commands.MissingPermissions):
+            elif isinstance(error, commands.MissingPermissions):
                 await ctx.send(self.bot.loc("events_error_u_cant"))
-            elif isinstance(e, commands.BotMissingPermissions):
+            elif isinstance(error, commands.BotMissingPermissions):
                 await ctx.send(self.bot.loc("events_error_b_cant"))
             else:
                 await ctx.send(self.bot.loc("events_error_generic"))
